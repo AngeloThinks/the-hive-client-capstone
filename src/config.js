dd25c6ba2f64fce0f28c38754e5e10276c7e4ac6
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-30 15:32:20 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    // API_ENDPOINT:'https://still-beach-15192.herokuapp.com/hiveapi',
-    API_ENDPOINT:'http://localhost:8000',
+    API_ENDPOINT:'https://shrouded-citadel-60157.herokuapp.com/hiveapi',
+    // API_ENDPOINT:'http://localhost:8000/hiveapi',
     TOKEN_KEY: 'spaced-repetition-jwt-secret'
 }
 
