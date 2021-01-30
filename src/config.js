87829ec5ab527f6e3bb5515e94752f6ee394cc84
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-29 22:40:52 -0600

    README

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     API_ENDPOINT:'https://still-beach-15192.herokuapp.com',
-    // API_ENDPOINT:'https://localhost:3000',
     TOKEN_KEY: 'spaced-repetition-jwt-secret'
 }
+
+// API_ENDPOINT:'https://localhost:8000',
